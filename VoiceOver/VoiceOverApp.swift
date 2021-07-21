1ee43a7a9b30a2a982600ba560177aaebf7e5791
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-07-21 19:30:14 +0500

    Initial Commit

---
+++ VoiceOver/VoiceOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceOverApp.swift
+//  VoiceOver
+//
+//  Created by Arkasha Zuev on 21.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct VoiceOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
